aa1a60bdc3113ca3d2f2fb75440abd04d11f4b68
khanh <khanh@Chungs-MacBook-Pro.local>
2019-11-22 11:06:05 +0700

    fix bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,7 @@ export default class extends Document {
           <meta name="format-detection" content="telephone=no, address=no, email=no" />
           <meta name="description" content={SITE_DESCRIPTION} />
           <meta name="keywords" content={SITE_KEYWORDS} />
+          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
           <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,Array.prototype.includes,String.prototype.includes,Array.prototype.findIndex,Object.entries"></script>
           {!DEV && FB_TRACKING_ID && (
             <React.Fragment>
