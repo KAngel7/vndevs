9e5a4f023d19f84e1d32f6ef5bc573b8ce4299f0
khanh <khanh@Chungs-MacBook-Pro.local>
2019-11-26 14:26:44 +0700

    add more css

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574744837433");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574753183371");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d6523c75488f887481f2";
+/******/ 	var hotCurrentHash = "a3eda6db6d43754f9516";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
